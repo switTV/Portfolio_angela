bf7711fddb0ba7b8f6dc5a6cc17287125ac5402d
swittv <4galarzajuan2004@gmail.com>
2023-03-07 13:38:28 -0300

    agregando las estadisticas

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel-elements').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: false,
+    slidesToShow: 2,
+    cssEase: 'linear'
+  });
+});
