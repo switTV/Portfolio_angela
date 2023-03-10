ee2b1187cf1848a1aafedba2dec18f066bec1679
swittv <4galarzajuan2004@gmail.com>
2023-03-10 15:34:54 -0300

    agregando el C A R R U S E L

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
-  $('.carousel-elements').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: false,
-    slidesToShow: 2,
-    cssEase: 'linear'
-  });
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: false,
+		dots: '.carousel__indicadores',
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 450,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				draggable: false,
+				
+			}
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 800,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4,
+				draggable: false,
+
+			  }
+			}
+		]
+	});
 });
