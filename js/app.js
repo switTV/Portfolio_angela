d3ee899683a97d15812a5552bf32837184ec75dc
switt_tv <4galarzajuan2004@gmail.com>
2023-03-14 13:20:16 -0600

    añadiendo el menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 let bars_nav = document.getElementById("bars_nav")
+let bars2 = document.getElementById("bars_2")
+let bg_black = document.getElementById("bg_black")
+
+let desplegable_menu = document.getElementById("desplegable-menu")
+
 bars_nav.addEventListener("click", () => {
-    console.log("hola")
+    desplegable_menu.classList.toggle("activado")
+    desplegable_menu.classList.remove("desactivado")
+    
+    bg_black.classList.toggle("activado")
+    bg_black.classList.remove("desactivado")
+
+})
+bars_2.addEventListener("click", () => {
+    desplegable_menu.classList.remove("activado")
+    desplegable_menu.classList.toggle("desactivado")
+
+    bg_black.classList.toggle("desactivado")
+    bg_black.classList.remove("activado")
+
 })
